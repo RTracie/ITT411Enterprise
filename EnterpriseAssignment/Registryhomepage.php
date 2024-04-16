b3b8f11b65e67dc81f53519d34797c73a32e1dc9
EmKare <126288958+EmKare@users.noreply.github.com>
2024-04-16 19:34:02 +0800

    Update Registryhomepage.php

--- EnterpriseAssignment/Registryhomepage.php
+++ EnterpriseAssignment/Registryhomepage.php
@@ -12,12 +12,10 @@ if (isset($_SESSION['Personelle'])) {
         background-color: #333;
         overflow: hidden;
         }
-
         .studentDrop, .lecturerDrop, .coursesDrop {
             float: left;
             overflow: hidden;
-        }      
-        
+        }
         .studentDrop .studentbtn, .lecturerDrop .lecturerbtn, .coursesDrop .coursebtn
         {
             font-size: 16px;  
@@ -29,12 +27,10 @@ if (isset($_SESSION['Personelle'])) {
             font-family: inherit;
             margin: 0;
         }
-
         .active:hover{
             background-color: #ddd;
-            color: #333;}
-
-
+            color: #333;
+	}
         .studentDropOptions, .lecturerDropOptions, .coursesDropOptions {
             display: none;
             position: absolute;
@@ -42,8 +38,7 @@ if (isset($_SESSION['Personelle'])) {
             min-width: 100px;
             box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
             z-index: 1;
-            }
-
+        }
         .studentDropOptions a, .lecturerDropOptions a, .coursesDropOptions a {
             float: none;
             color: black;
@@ -51,30 +46,27 @@ if (isset($_SESSION['Personelle'])) {
             text-decoration: none;
             display: block;
             text-align: left;
-            }
-            
+        }            
         .studentDropOptions a:hover, .lecturerDropOptions a:hover,.coursesDropOptions a:hover{
             background-color: #ddd;
         }
-
         .studentDrop:hover .studentDropOptions, .lecturerDrop:hover .lecturerDropOptions,.coursesDrop:hover .coursesDropOptions, .active:hover {
             display:block;
         }
-
-        .studentDrop:hover .studentbtn, .lecturerDrop:hover .lecturerbtn, .coursesDrop:hover .coursebtn {background-color: black;}
-
+        .studentDrop:hover .studentbtn, .lecturerDrop:hover .lecturerbtn, .coursesDrop:hover .coursebtn {
+	    background-color: black;
+	}
         .active{
-            background-color: red;}
-
-            .active{
+            background-color: red;
+	}
+        .active{
             color:#f2f2f2;
             float: left;
             text-align: center;
             padding: 14px 16px;
             text-decoration: none;
             font-size: 16px;
-        }
-        
+        }        
      </style>
      <script>
         function viewAllStudents()
@@ -97,8 +89,8 @@ if (isset($_SESSION['Personelle'])) {
         function searchCourses(){
             document.getElementById("workspace").innerHTML='<object type="text/html" data="searchCourse.php" style="width:100%; height:100%;"></object>>'
         }
-        function personalinfo(){
-            document.getElementById("workspace").innerHTML='<object type="text/html" data="studentpersonal.php" style="width:100%; height:100%;"></object>>'
+        function personalinfo(){ //not used, can be removed or used if you see fit
+            document.getElementById("workspace").innerHTML='<object type="text/html" data=" " style="width:100%; height:100%;"></object>>'
         }
     </script>
 </head>
@@ -128,14 +120,12 @@ if (isset($_SESSION['Personelle'])) {
                     <a href="javascript:searchCourses()"> Search</a>
                 </div>
             </div>
-            <!--<a href="javascript:personalinfo()">My Details</a> -->
             <a style="float:right" class="active" href="logoutUser.php">Logout</a>
         </div>          
      </div>
      <div id="workspace"></div>
 </body>
 </html>
-
 <?php 
 }else{
      header("Location: studentlogin.php");
