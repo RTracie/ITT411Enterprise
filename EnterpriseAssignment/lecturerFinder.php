4c6e7e393a6c6b1a2ade608a31531702f9ee7938
EmKare <126288958+EmKare@users.noreply.github.com>
2024-04-16 19:38:37 +0800

    Update lecturerFinder.php

--- EnterpriseAssignment/lecturerFinder.php
+++ EnterpriseAssignment/lecturerFinder.php
@@ -1,6 +1,6 @@
 <?php
 	include "connect.php";
-    session_start();
+    	session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,17 +8,17 @@
     </head>
     <body>
         <h1>Search Lecturers</h1>
-    <?php
-        $findquery="select * from Lecturers where lecturerID='$_POST[search]'";
-        $result=mysqli_query($connection,$findquery)or die('Error query not working');
-        if($result->num_rows>0)
-        {
-            while($row=$result->fetch_assoc())
-            { 
-                $_SESSION['lecturerID'] = $row['lecturerID'];
-            }
-        } 
-        header("Location: viewSingleLecturer.php?id=$_SESSION[lecturerID]");
-    ?>        
+	    <?php
+	        $findquery="select * from Lecturers where lecturerID='$_POST[search]'";
+	        $result=mysqli_query($connection,$findquery)or die('Error query not working');
+	        if($result->num_rows>0)
+	        {
+	            while($row=$result->fetch_assoc())
+	            { 
+	                $_SESSION['lecturerID'] = $row['lecturerID'];
+	            }
+	        } 
+	        header("Location: viewSingleLecturer.php?id=$_SESSION[lecturerID]");
+	    ?>        
     </body>
 </html>
