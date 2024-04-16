aa3729fa43faa7c99a7692e0c4d45779891db35d
EmKare <126288958+EmKare@users.noreply.github.com>
2024-04-16 17:11:45 +0800

    Update connect.php

--- EnterpriseAssignment/connect.php
+++ EnterpriseAssignment/connect.php
@@ -1,7 +1,7 @@
 <?php
     define("database","Registry_Department");
-    define("user","root"); #add user if necessary
-    define("password","1234"); #add password if necessary
+    define("user","----"); #add user if necessary
+    define("password","----"); #add password if necessary
 	
 	$connection=mysqli_connect('localhost',user,password,database) or die('Error connecting to MYSQL');
 
