c5b865d4306e5b9fa56f08f9302d6ef97cc079c7
EmKare <126288958+EmKare@users.noreply.github.com>
2024-04-16 19:46:43 +0800

    Update studentFinder.php

--- EnterpriseAssignment/studentFinder.php
+++ EnterpriseAssignment/studentFinder.php
@@ -1,5 +1,5 @@
 <?php
-	include "connect.php";
+    include "connect.php";
     session_start();
 ?>
 <!DOCTYPE html>
@@ -8,17 +8,17 @@
     </head>
     <body>
         <h1>Search Students</h1>
-    <?php
-        $findquery="select * from Students where studentID='$_POST[search]'";
-        $result=mysqli_query($connection,$findquery)or die('Error query not working');
-        if($result->num_rows>0)
-        {
-            while($row=$result->fetch_assoc())
-            { 
-                $_SESSION['studentID'] = $row['studentID'];
-            }
-        } 
-        header("Location: viewSingleStudent.php?id=$_SESSION[studentID]");
-    ?>        
+    	<?php
+	    $findquery="select * from Students where studentID='$_POST[search]'";
+	    $result=mysqli_query($connection,$findquery)or die('Error query not working');
+	     if($result->num_rows>0)
+	      {
+	            while($row=$result->fetch_assoc())
+	            { 
+	                $_SESSION['studentID'] = $row['studentID'];
+	            }
+	      } 
+	      header("Location: viewSingleStudent.php?id=$_SESSION[studentID]");
+	?>        
     </body>
 </html>
