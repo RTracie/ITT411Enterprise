408abc76532d6a6c8b158da16ddbac647aa882a9
EmKare <126288958+EmKare@users.noreply.github.com>
2024-04-16 19:44:21 +0800

    Update searchLecturer.php

--- EnterpriseAssignment/searchLecturer.php
+++ EnterpriseAssignment/searchLecturer.php
@@ -7,8 +7,8 @@
     <head>        
         <meta charset="utf-8">
         <title>Search</title>
-       <link rel="sty lesheet" href="style.css">
-       <style>
+        <link rel="sty lesheet" href="style.css">
+        <style>
             #search{border-radius: 8px;padding: 5px 6px;margin: 4px 0}
             P{font-size: 21px;}
             input[type="submit"]{border-radius: 10px;padding:3px 5px;margin-top: 5px;}
@@ -16,9 +16,7 @@
     </head>
     <body>
         <div class="center">
-
-        <h1>Search Lecturer</h1>
-
+            <h1>Search Lecturer</h1>
             <form action="lecturerFinder.php" method="POST">
                 <div>
                     <p>Search by Lecturer ID</p>
