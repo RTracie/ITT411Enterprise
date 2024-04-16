cb1b06b5f2154aa8cadb36ef366317fe4c445bd5
EmKare <126288958+EmKare@users.noreply.github.com>
2024-04-16 19:43:42 +0800

    Update searchCourse.php

--- EnterpriseAssignment/searchCourse.php
+++ EnterpriseAssignment/searchCourse.php
@@ -7,24 +7,24 @@
     <head>        
         <meta charset="utf-8">
         <title>Search</title>
-       <link rel="sty lesheet" href="style.css">
-       <style>
+        <link rel="sty lesheet" href="style.css">
+        <style>
             #search{border-radius: 8px;padding: 5px 6px;margin: 4px 0}
             P{font-size: 21px;}
             input[type="submit"]{border-radius: 10px;padding:3px 5px;margin-top: 5px;}
-       </style>
+        </style>
     </head>
     <body>
         <div class="center">
 
-        <h1>Search Course</h1>
+            <h1>Search Course</h1>
 
             <form action="sectionFinder.php" method="POST">
                 <div>
                     <p>Search by Course Code</p>
                     <input type="text" name="search" id="search" value="" placeholder="Search..." required> 
                 </div>              
-                    <div>
+                <div>
                     <input type ="submit" name="submit" value="Search"/>
                 </div><br><br>
             </form>
